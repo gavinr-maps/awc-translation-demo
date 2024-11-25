99ed43fd6514834c539143f458bbd8e2475eed34
Gavin Rehkemper <grehkemper@esri.com>
2024-11-25 11:34:17 -0600

    add translation

--- src/components/hello-world/hello-world.tsx
+++ src/components/hello-world/hello-world.tsx
@@ -1,4 +1,6 @@
 import { LitElement, h, property, type JsxNode } from "@arcgis/lumina";
+import type T9nStrings from "./assets/t9n/hello-world.t9n.en.json";
+import { useT9n } from "../../controllers/useT9n";
 
 declare global {
   interface DeclareElements {
@@ -12,12 +14,18 @@ export class ArcgisHelloWorld extends LitElement {
 
   @property() name = "World";
 
+  @property() messageOverrides?: typeof this._messages._overrides;
+
+  //#endregion
+
+  //#region Private Properties
+  private _messages = useT9n<typeof T9nStrings>();
   //#endregion
 
   //#region Rendering
 
   override render(): JsxNode {
-    return <calcite-button>Hello {this.name}!</calcite-button>;
+    return <calcite-button>{this._messages.hello} {this.name}!</calcite-button>;
   }
 
   //#endregion
